44f19a06a770f2052a097a2bcb61ce4d870b01d1
mgilesy <mike@gilesonline.com>
2023-05-07 11:05:53 +1000

    first days update

--- src/runCollection.ts
+++ src/runCollection.ts
@@ -1,13 +1,19 @@
 import { createMailSlurpAccount, MailSlurpAccount } from "./services/mailSlurp";
+import createExternalUser, { createExternalUserParams } from "./services/users";
+import generateName from "./services/";
 
 async function runCollection() {
   console.log("About to run first create mail slurp account");
   let account = await createMailSlurpAccount();
   console.log("Completed creating first mail slurp account", account);
 
-  console.log("About to run second create mail slurp account");
-  account = await createMailSlurpAccount();
-  console.log("Completed creating second mail slurp account", account);
+  console.log("About to run create users");
+  // let userParams = <createExternalUserParams>{};
+  // userParams.email = account.emailAddress;
+  // userParams.firstName = "Mike" +
+  // console.log("Completed creating second mail slurp account", account);
+  const name = await generateName("male");
+  console.log("random name: ", name);
 }
 
 export default runCollection;
